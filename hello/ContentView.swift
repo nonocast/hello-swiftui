83dfe00dd476f9ec04f539cac0227aff82bb0ea0
Hui Cao <nonocast@gmail.com>
2019-10-05 21:00:09 +0800

    Initial Commit

---
+++ hello/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  hello
+//
+//  Created by h705c on 2019/10/5.
+//  Copyright © 2019 shgbit. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
